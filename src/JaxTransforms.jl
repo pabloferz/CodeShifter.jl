c397054215543e5e2b93b6430b1d83ec50c3ab0a
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2022-10-02 13:23:21 -0500

    Refine reachability analysis pass

--- src/JaxTransforms.jl
+++ src/JaxTransforms.jl
@@ -12,10 +12,10 @@ include("ForwardRules.jl")
 
 import Base.Meta: isexpr
 import Core: ReturnNode, SlotNumber, SSAValue
-import .CodeShifter: @ns, FunTransform, TransformContext, process_inputs, transform!
+import .CodeShifter:
+    @ns, FunTransform, TransformContext,
+    is_primitive, process_inputs, transform!
 import .ForwardRules: c_InactivePrimitives
-
-const Itrs = Iterators
 
 
 # Type definitions
@@ -49,74 +49,70 @@ function is_primitive(::Type{JVP}, F, Args...)
 end
 
 function transform!(::JVP, ci, meth, nargs, offset, sparams)
-    new_code = Any[]
-    new_codelocs = Any[]
+    code = Any[]
     ssa_mapping = Int[]
-    loc_mapping = Int[]
 
     function emit!(stmt)
         (isexpr(stmt, :call) || isexpr(stmt, :(=)) || isexpr(stmt, :new)) || return stmt
-        push!(new_code, stmt)
-        push!(new_codelocs, isempty(new_codelocs) ? 0 : new_codelocs[end])
-        SSAValue(length(new_code))
+        push!(code, stmt)
+        # push!(new_codelocs, isempty(new_codelocs) ? 0 : new_codelocs[end])
+        SSAValue(length(code))
     end
 
     tags_set = transformable_ids(ci.code, nargs)
 
     for i in 2:nargs
-        ci.code[i] = Expr(:call, ci.code[i].args[1], SlotNumber(3), i - 1)
+        # ci.code[i] = Expr(:call, ci.code[i].args[1], SlotNumber(3), i - 1)
     end
 
-    insert!(ci.code, 2, Expr(:(=), SlotNumber(4), Expr(:call, getfield, SlotNumber(2), 2)))
-    insert!(ci.code, 2, Expr(:(=), SlotNumber(3), Expr(:call, getfield, SlotNumber(2), 1)))
+    for (id, stmt) in enumerate(ci.code)
+        if SSAValue(id) in tags_set
+            push!(code, stmt)
+        else
+            push!(code, stmt)
+        end
+    end
 
-    insert!(ci.slotnames, 3, :tangents)
-    insert!(ci.slotnames, 3, :primals)
+    ci.code = code
+    # insert!(ci.code, 2, Expr(:(=), SlotNumber(4), Expr(:call, getfield, SlotNumber(2), 2)))
+    # insert!(ci.code, 2, Expr(:(=), SlotNumber(3), Expr(:call, getfield, SlotNumber(2), 1)))
 
-    insert!(ci.slotflags, 3, 0x00)
-    insert!(ci.slotflags, 3, 0x00)
+    # insert!(ci.slotnames, 3, :tangents)
+    # insert!(ci.slotnames, 3, :primals)
 
-    # for (i, stmt) in enumerate(new_code)
-    #     if isa(stmt, GotoNode)
-    #         new_code[i] = GotoNode(loc_mapping[stmt.label])
-    #     elseif isa(stmt, GotoIfNot)
-    #         new_code[i] = GotoIfNot(stmt.cond, loc_mapping[stmt.dest])
-    #     end
-    # end
+    # insert!(ci.slotflags, 3, 0x00)
+    # insert!(ci.slotflags, 3, 0x00)
 
     return ci, nargs, offset + 1
 end
 
 function transformable_ids(code, nargs)
     tags_set = Set{IRTag}()
-    for (id, stmt) in Itrs.reverse(pairs(code))
-        collect_tags_bwd!(tags_set, stmt, id)
+    for (id, stmt) in Iterators.reverse(pairs(code))
+        collect_tags_rev!(tags_set, stmt, id)
     end
-    intersect!(tags_set, (SSAValue(i) for i = 1:nargs))
-    for (id, stmt) in Itrs.drop(pairs(code), nargs)
+    intersect!(tags_set, (SlotNumber(i) for i = nargs+1:2nargs-1))
+    for (id, stmt) in Iterators.drop(pairs(code), nargs)
         collect_tags_fwd!(tags_set, stmt, id)
     end
-    return tags_set
+    return filter!(t -> t isa SSAValue, tags_set)
 end
 
-function collect_tags_bwd!(ids_set, stmt, id)
+function collect_tags_rev!(ids_set, stmt, id)
     if isa(stmt, ReturnNode) && isa(stmt.val, IRTag)
         push!(ids_set, stmt.val)
-    elseif isa(stmt, IRTag) && SSAValue(id) in ids_set
+    elseif isa(stmt, IRTag)
         push!(ids_set, stmt)
-    elseif isexpr(stmt, :call) && !(stmt.args[1] in c_InactivePrimitives) && SSAValue(id) in ids_set
+    elseif isexpr(stmt, :call) && !(stmt.args[1] in c_InactivePrimitives)
         ind = isa(stmt.args[1], IRTag) ? 1 : 2
-        foreach(stmt -> collect_tags_bwd!(ids_set, stmt, id), @view stmt.args[ind:end])
+        foreach(stmt -> collect_tags_rev!(ids_set, stmt, id), @view stmt.args[ind:end])
     elseif isexpr(stmt, :(=)) && stmt.args[1] in ids_set
-        push!(ids_set, SSAValue(id))
-        foreach(stmt -> collect_tags_bwd!(ids_set, stmt, id), @view stmt.args[2:end])
+        foreach(stmt -> collect_tags_rev!(ids_set, stmt, id), @view stmt.args[2:end])
     end
 end
 
 function collect_tags_fwd!(ids_set, stmt, id)
-    if isa(stmt, ReturnNode) && stmt.val in ids_set
-        push!(ids_set, SSAValue(id))
-    elseif isa(stmt, IRTag) && stmt in ids_set
+    if isa(stmt, IRTag) && stmt in ids_set
         push!(ids_set, SSAValue(id))
     elseif isexpr(stmt, :call) && !(stmt.args[1] in c_InactivePrimitives)
         ind = isa(stmt.args[1], IRTag) ? 1 : 2
